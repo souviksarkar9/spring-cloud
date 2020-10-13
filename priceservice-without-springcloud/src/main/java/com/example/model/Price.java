83313fb4f8521910d44090f408f2351803ab17f6
souviksarkar9 <souviksarkar9@gmail.com>
2020-10-12 21:34:57 -0500

    priceservice-without-springcloud

---
+++ priceservice-without-springcloud/src/main/java/com/example/model/Price.java
@@ -0,0 +1,40 @@
+package com.example.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Price {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String code;
+	private BigDecimal amount;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public BigDecimal getAmount() {
+		return amount;
+	}
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+	
+	
+	
+}
